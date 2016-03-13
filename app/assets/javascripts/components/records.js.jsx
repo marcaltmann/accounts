74b7f4310b8a05016f9d862179260900831adf3c
Marc Altmann <mail@marcaltmann.com>
2016-03-14 00:40:40 +0100

    Add first React component

---
+++ app/assets/javascripts/components/records.js.jsx
@@ -0,0 +1,10 @@
+
+this.Records = React.createClass({
+  render: function() {
+    return (
+      <div className="records">
+        <h2 className="title">Records</h2>
+      </div>
+    );
+  }
+});
